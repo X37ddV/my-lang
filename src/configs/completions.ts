96274248d8f955bf02624d30f836b32869aa1d61
ChenGang <X37ddV@iCloud.com>
2024-03-16 18:37:36 +0800

    Delete unused icon files

--- src/configs/completions.ts
+++
@@ -1,7 +0,0 @@
-import { functions } from "./functions";
-import { keywords } from "./keywords";
-
-export const completions = [...functions, ...keywords];
-export const completionMap = new Map(
-    completions.map((completion) => [completion.label, completion])
-);
